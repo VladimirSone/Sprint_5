2c524efe89d9e6a3acd335be4e7b3becc188784f
Vladimir Shutov-Troitskiy <WASH85@mail.ru>
2024-09-03 22:54:20 +0300

    first commit

--- helpers.py
+++ helpers.py
@@ -15,13 +15,6 @@ def create_random_password():
     random_password = f'qwerty_{random_letters}_{random.randint(10, 99)}'
     return random_password
 
-# Генерация основы email с четырьмя рандомными буквами и рандомным числом от 100 до 999
-# для негативных проверок на формат вводимого адреса почты
-# def create_email_base():
-    # random_letters = ''.join((random.choice(string.ascii_letters) for x in range(4)))
-    # email_base = f'vladimirshutovtroitskiy{random_letters}_{random.randint(100, 999)}'
-    # return email_base
-
 # Генерация пароля с невалидными значениями
 def randompassword():
   chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
